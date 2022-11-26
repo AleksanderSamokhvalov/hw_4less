831978d12a081dcdfa8a9dc8f6e3f08a63eb2580
Mr.Poxy/\b <solom.solom2910@gmail.com>
2022-11-26 23:16:02 +0300

    task_29_done!

--- hw_task_29/Program.cs
+++ hw_task_29/Program.cs
@@ -4,7 +4,12 @@
 
 // 6, 1, 33 -> [6, 1, 33]
 
-int [] array = new int [8];
+Console.WriteLine ("Программа для вывода массива, количество элесентов которого равно (А). V 1.0");
+
+Console.WriteLine ("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [A];
 GetArray(array);
 
 int [] GetArray(int [] array)
@@ -13,10 +18,11 @@ int [] GetArray(int [] array)
 
 for (int i = 0; i <array.Length; i++)
 {
-array[i] = new Random().Next(0,2);
+array[i] = new Random().Next(0,99);
 }
 
 return array;
 }
 
 Console.Write(String.Join(",", array));
+
