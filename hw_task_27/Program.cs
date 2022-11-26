83516700dd8840829bc7dc302ec09e0b372011c7
Mr.Poxy/\b <solom.solom2910@gmail.com>
2022-11-26 23:39:04 +0300

    task_27_done!

--- hw_task_27/Program.cs
+++ hw_task_27/Program.cs
@@ -5,3 +5,21 @@
 // 82 -> 10
 
 // 9012 -> 12
+
+Console.WriteLine ("Программа для вывода суммы каждого элемента в (А). V 1.0");
+
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int summ = 0;
+
+while (A>0)
+{
+    summ = summ +A%10;
+    A = A/10;
+}
+
+Console.WriteLine("Сумма элементов числа " + summ);
+
+
+
